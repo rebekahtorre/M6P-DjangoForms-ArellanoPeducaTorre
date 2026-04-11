180d6b1dcf02498a99d12b155a487473b26f78b1
karlpeduca <karl.nicholai.peduca@student.ateneo,edu>
2026-04-11 23:59:41 +0800

    uhhh

--- MyInventorySystem/MyInventoryApp/urls.py
+++ MyInventorySystem/MyInventoryApp/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 
     path('home/', views.home, name='home'),
 
-    path('view_supplier/<int:pk>/', views.view_supplier, name='view_supplier'),
+    path('view_supplier/', views.view_supplier, name='view_supplier'),
     path('view_bottles/', views.view_bottles, name='view_bottles'),
     path('add_bottle/', views.add_bottle, name='add_bottle'),
 
--- MyInventorySystem/MyInventoryApp/views.py
+++ MyInventorySystem/MyInventoryApp/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Supplier, WaterBottle, Account
 
+current_pk = None
+
 def home(request):
-    return render(request, 'MyInventoryApp/base.html')
+    return render(request, 'MyInventoryApp/base.html', {"pk": current_pk})
 
 def add_bottle(request):
-    return render(request, 'MyInventoryApp/add_bottle.html')
+    return render(request, 'MyInventoryApp/add_bottle.html', {"pk": current_pk})
 
-def view_supplier(request, pk):
+def view_supplier(request):
     suppliers = Supplier.objects.all()
-    return render(request, 'MyInventoryApp/view_supplier.html', {"suppliers": suppliers, "pk": pk})
+    return render(request, 'MyInventoryApp/view_supplier.html', {"suppliers": suppliers, "pk": current_pk})
 
 def view_bottles(request):
     bottles = WaterBottle.objects.all()
-    return render(request, 'MyInventoryApp/view_bottles.html', {"bottles": bottles})
+    return render(request, 'MyInventoryApp/view_bottles.html', {"bottles": bottles, "pk": current_pk})
 
 def login(request):
     if request.method == "POST":
@@ -23,7 +25,9 @@ def login(request):
         acc_qs = Account.objects.filter(username=username, password=password)
         if acc_qs:
             acc = acc_qs[0]
-            return redirect('view_supplier', pk=acc.pk)
+            global current_pk
+            current_pk = acc.pk
+            return redirect('view_supplier')
         else:
             return render(request, "MyInventoryApp/login.html", {"error": "Invalid login"})
 
@@ -43,11 +47,11 @@ def signup(request):
     return render(request, 'MyInventoryApp/signup.html')
 
 def manage_account(request, pk):
-    acc = get_object_or_404(Account, pk=pk)
-    return render(request, "MyInventoryApp/manage.html", {"acc": acc})
+    acc = get_object_or_404(Account, pk=current_pk)
+    return render(request, "MyInventoryApp/manage.html", {"acc": acc, "pk": current_pk})
 
 def change_password(request, pk):
-    acc = get_object_or_404(Account, pk=pk)
+    acc = get_object_or_404(Account, pk=current_pk)
 
     if request.method == "POST":
         current = request.POST.get("current_password")
@@ -55,16 +59,20 @@ def change_password(request, pk):
         new2 = request.POST.get("new_password2")
 
         if current == acc.password and new1 == new2:
-            Account.objects.filter(pk=pk).update(password=new1)
-            return redirect('manage_account', pk=pk)
+            Account.objects.filter(pk=current_pk).update(password=new1)
+            return redirect('manage_account', pk=current_pk)
         else:
-            return render(request, "MyInventoryApp/change_password.html", {"acc": acc, "message": "Invalid password change"})
+            return render(request, "MyInventoryApp/change_password.html", {"acc": acc, "message": "Invalid password change", "pk": current_pk})
 
-    return render(request, "MyInventoryApp/change_password.html", {"acc": acc})
+    return render(request, "MyInventoryApp/change_password.html", {"acc": acc, "pk": current_pk})
 
 def delete_account(request, pk):
-    Account.objects.filter(pk=pk).delete()
+    global current_pk
+    Account.objects.filter(pk=current_pk).delete()
+    current_pk = None
     return redirect('login')
 
 def logout(request):
+    global current_pk
+    current_pk = None
     return redirect('login')
